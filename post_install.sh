542dc68d2fc5b73967a6ed7f00dd1f0a4721e8b7
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-07-19 23:10:00 +0200

    More plugin compatibility patches

--- post_install.sh
+++ post_install.sh
@@ -1,9 +1,15 @@
-sysrc -f /etc/rc.conf  radarr_enable="YES"
+#!/usr/local/bin/bash
 
-#Ticket #62991
-chown -R radarr:radarr /usr/local/share/radarr
+pw user add radarr -c radarr -u 352 -d /nonexistent -s /usr/bin/nologin
 
-#we need write permission to be able to write plugins update. #53127
+FILE_NAME=$(curl -s https://api.github.com/repos/Radarr/Radarr/releases | jq -r '[[.[] | select(.draft != true) | select(.prerelease == true)][0] | .assets | .[] | select(.name | endswith(".linux.tar.gz")) | .name][0]')
+DOWNLOAD=$(curl -s https://api.github.com/repos/Radarr/Radarr/releases | jq -r '[[.[] | select(.draft != true) | select(.prerelease == true)][0] | .assets | .[] | select(.name | endswith(".linux.tar.gz")) | .browser_download_url][0]')
+
+
+iocage exec "${1}" fetch -o /usr/local/share "${DOWNLOAD}"
+tar -xzvf /usr/local/share/${FILE_NAME} -C /usr/local/share
+rm /usr/local/share/"${FILE_NAME}"
 chmod 755 /usr/local/radarr
+chmod u+x /usr/local/etc/rc.d/radarr
+sysrc "radarr_enable=YES"
 
-service radarr start
