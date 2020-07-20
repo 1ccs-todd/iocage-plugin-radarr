c10390edeed9f59fa0d06ddc6f99541b6d4716b1
kjeld Schouten-Lebbing <kjeld@schouten-lebbing.nl>
2020-07-20 22:29:31 +0200

    Fix some install bugs

--- post_install.sh
+++ post_install.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-pw user add radarr -c radarr -u 352 -d /nonexistent -s /usr/bin/nologin
-
 FILE_NAME=$(curl -s https://api.github.com/repos/Radarr/Radarr/releases | jq -r '[[.[] | select(.draft != true) | select(.prerelease == true)][0] | .assets | .[] | select(.name | endswith(".linux.tar.gz")) | .name][0]')
 DOWNLOAD=$(curl -s https://api.github.com/repos/Radarr/Radarr/releases | jq -r '[[.[] | select(.draft != true) | select(.prerelease == true)][0] | .assets | .[] | select(.name | endswith(".linux.tar.gz")) | .browser_download_url][0]')
 
-
-iocage exec "${1}" fetch -o /usr/local/share "${DOWNLOAD}"
+pw user add radarr -c radarr -u 352 -d /nonexistent -s /usr/bin/nologin
+fetch -o /usr/local/share "${DOWNLOAD}"
 tar -xzvf /usr/local/share/${FILE_NAME} -C /usr/local/share
 rm /usr/local/share/"${FILE_NAME}"
 chmod 755 /usr/local/radarr
